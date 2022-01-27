85cb5e30ee8ae4c0a270b69c11ba853b07c005f2
Марианна Игнашкина <maritiger130201@gmail.com>
2022-01-27 09:16:24 +0200

    First version

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const React = require('react');
+import { hot } from 'react-hot-loader/root'
+
+export const App = hot(_App)
+export function _App(): JSX.Element | null {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
